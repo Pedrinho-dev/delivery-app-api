17b43a1aa0cfd91f7f960af2b0c41f7b039c052e
Joao Vitor Assis <JoaaoVitorAssis@proton.me>
2025-11-22 18:23:57 -0400

    feat: :sparkles: Create orderSchema and Route order

--- server.js
+++ server.js
@@ -3,27 +3,31 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoute.js";
 import authRoute from "./routes/authRoute.js";
+import orderRoute from "./routes/orderRoute.js";
 
 dotenv.config();
 
-const port = process.env.PORT
-const db = process.env.MONGO_URI
+const port = process.env.PORT;
+const db = process.env.MONGO_URI;
 const server = express();
 
 server.use(express.json());
 
 server.use("/user", userRoute);
 server.use("/auth", authRoute);
+server.use("/order", orderRoute);
 
 server.get("/", (req, res) => {
-    res.send("IM RUNNIG THE GET ROUTE!!!")
-})
+  res.send("IM RUNNIG THE GET ROUTE!!!");
+});
 
-mongoose.connect(db).then(() => {
+mongoose
+  .connect(db)
+  .then(() => {
     server.listen(port, () => {
-        console.log(`http://localhost:${port}`)
-    })
-}).catch(err => {
-    console.log(err)
-})
-
+      console.log(`http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
