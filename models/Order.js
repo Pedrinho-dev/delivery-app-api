764ba8259667f6e670530d72c28b2f67c358a3c5
Pedrinho-dev <pedroantoniofalves@gmail.com>
2025-12-04 17:12:43 -0400

    feat: :sparkles: add finalLoc

--- models/Order.js
+++ models/Order.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema({
   clientLoc: { type: String },
   destinyLoc: { type: String },
+  locDeliveryMan: { type: String },
   schedule: { type: Date },
   accept: { type: Boolean, default: false },
+  finalLoc: { type: Boolean, default: false },
   idClient: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  locDeliveryMan: { type: String },
 });
 
 const Order = mongoose.model("Order", orderSchema);
